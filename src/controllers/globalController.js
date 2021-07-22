7dd6d0c408590a572248b1caa606cabc042da4b7
ouhouh1936 <ouhouh1936@gmail.com>
2021-07-22 21:45:18 +0900

    main.pug login

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -2,6 +2,21 @@ export const testScreen = (req, res) => {
   res.render("base");
 };
 
+export const mainController = (req, res) => {
+  const LoginFlag = req.userLoginFlag || false;
+
+  let isAuthemticated = false;
+
+  if (LoginFlag) {
+    isAuthemticated = true;
+  }
+
+  if (isAuthemticated) {
+    friendController(req, res);
+  } else {
+    res.render("main");
+  }
+};
 export const friendController = (req, res) => {
   res.render("friends");
 };
@@ -12,4 +27,19 @@ export const messageController = (req, res) => {
 
 export const profileController = (req, res) => {
   res.render("profile");
+};
+
+export const loginController = (req, res) => {
+  const input_id = req.body.input_id;
+  let input_pass = req.body.input_pass;
+  input_pass = String(input_pass);
+
+  if (input_id === "system" && input_pass === "1234") {
+    //로그인성공
+    req.userLoginFlag = true;
+    mainController(req, res);
+  } else {
+    req.userLoginFlag = false;
+    mainController(req, res);
+  }
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,15 +1,19 @@
 import express from "express";
 import {
-  testScreen,
   friendController,
   messageController,
   profileController,
+  loginController,
+  mainController,
 } from "../controllers/globalController";
+
 const globalRouter = express.Router();
 
-globalRouter.get("/", testScreen);
+globalRouter.get("/", mainController);
 globalRouter.get("/friends", friendController);
 globalRouter.get("/message", messageController);
 globalRouter.get("/profile", profileController);
+
+globalRouter.post("/login", loginController);
 
 export default globalRouter;
